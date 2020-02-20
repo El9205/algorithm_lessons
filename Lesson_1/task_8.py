30a0e8443f620033865007f16f740c84fbdc7f0b
El9205 <el9205@bk.ru>
2020-02-21 01:44:56 +0300

    Проверка

--- Lesson_1/task_8.py
+++
@@ -1,9 +0,0 @@
-# https://drive.google.com/file/d/1rNBjCfIebM1V2tFALqzYf1UY12JX-4D4/view?usp=sharing
-#Определить, является ли год, который ввел пользователь, високосным или не високосным.
-
-a = int(input('Введите год: '))
-
-if a % 4 == 0:
-    print ("Год високосный")
-else:
-    print ('Год не високосный')
